38a0fe8fdd4b787c0222b734ba070e07aaf2e93e
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-10-14 18:45:49 +0100

    add widgets

---
+++ frontend/src/components/widget/Widget.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./widget.css"
+
+function Widget() {
+  return (
+    <div className='widget'>
+     <div className="widget__title">
+       Number of youths
+     </div>
+     <div className="widget__data">
+      50
+     </div>
+    </div>
+  )
+}
+
+export default Widget
--- frontend/src/pages/youth/Youth.jsx
+++ frontend/src/pages/youth/Youth.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import NavigationTab from '../../components/tabs/NavigationTab'
+import NavigationTab from '../../components/navigationTab/NavigationTab'
+import Widget from '../../components/widget/Widget'
 import "./youth.css"
 
 export const Youth = () => {
   return (
    <div>
-   <NavigationTab/>
+   <NavigationTab/> 
+   <Widget/>
    </div>
    
   )
